83c74e12e343c666dd234688df3c2a8c546da3dc
RoySalgadoM <20203tn052@utez.edu.mx>
2023-03-18 16:40:39 -0600

    Starting project

---
+++ src/test/java/mx/edu/utez/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
