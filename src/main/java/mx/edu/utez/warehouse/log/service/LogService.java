8f82b6bba15aae4f4c7c9baf67a23f9d3051ed31
Roy Salgado <roy.salgado@exos.mx>
2023-04-10 09:07:53 -0600

    [UPDATE] Logs view

--- src/main/java/mx/edu/utez/warehouse/log/service/LogService.java
+++ src/main/java/mx/edu/utez/warehouse/log/service/LogService.java
@@ -1,5 +1,10 @@
 package mx.edu.utez.warehouse.log.service;
 
+import mx.edu.utez.warehouse.message.model.MessageModel;
+import org.springframework.data.domain.Pageable;
+
 public interface LogService {
+    MessageModel findAllLogs(Pageable page, String username, String uuid);
+
     void saveLog(String action, Long idAction, String username, String uuid);
 }
